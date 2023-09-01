90e3c4f8d2d7d608145214ee134c3e06673a836c
Daniel Ekman <knegge@gmail.com>
2023-09-01 20:18:48 +0200

    Fix calculations and timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #define LED 13
-#define DIV 62500
+#define DIV 62500           // 1M / 16 = 62500
 
 int count, time, time_old, count_old;
 long freq;
@@ -9,11 +9,15 @@ bool trig = true;
 void setup() {
   pinMode(LED, OUTPUT);
   Serial.begin(9600);
-
+  Serial.println("freq timer");
   TCCR1A = 0;
-  TCCR1B = (1 << WGM12) | (1 << CS10);       // CTC mode, no prescaler
-  OCR1A = DIV;                               // 1M / 16 = 62500
-  TIMSK1 |= (1 << OCIE1A) | (1 << ICIE1);    // enable timer compare and capture interrupt on PB0 (arduino pin 8)
+  TCCR1B = (1 << WGM12)     // CTC mode
+         | (1 << CS10)      // no prescaler
+         | (1 << ICES1)     // timer capture input
+         | (1 << ICNC1);    // rising edge
+  OCR1A = DIV - 1;          // set timer top
+  TIMSK1 = (1 << OCIE1A)    // enable timer compare interrupt
+         | (1 << ICIE1);    // enable capture interrupt on PB0 (arduino pin 8)
   sei();
 }
 
@@ -22,7 +26,7 @@ void loop() {
     trig = false;
     Serial.print(freq);
     Serial.print(" ");
-    Serial.println(count);
+    Serial.println(time);
     if(digitalRead(LED)){
       digitalWrite(LED,LOW);
     } else {
